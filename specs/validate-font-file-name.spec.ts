0fea23735377ab0cabad35a1c0717c3d54b857bb
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-08-15 15:54:58 +0300

    spec: rewrite

--- specs/validate-font-file-name.spec.ts
+++ specs/validate-font-file-name.spec.ts
@@ -11,8 +11,8 @@ describe('Validate Font File Name', () => {
       equal: boolean,
       regex?: string | RegExp,
     ) => {
-      return validateFontFileName({ file, regex }).then((parameter) => {
-        expect(parameter).equal(equal);
+      return await validateFontFileName({ file, regex }).then((parameters) => {
+        expect(parameters).equal(equal);
       });
     };
 
@@ -20,15 +20,19 @@ describe('Validate Font File Name', () => {
   });
 
   spec('OpenSans-SemiBold.woff2', async () => {
-    return validate('OpenSans-SemiBold.woff2', true);
+    return await validate('OpenSans-SemiBold.woff2', true);
   });
 
   spec('regex: /OpenSans-SemiBold\\.woff2/', async () => {
-    return validate('OpenSans-SemiBold.woff2', true, /OpenSans-SemiBold\.woff2/);
+    return await validate(
+      'OpenSans-SemiBold.woff2',
+      true,
+      /OpenSans-SemiBold\.woff2/,
+    );
   });
 
   spec("regex: new RegExp('OpenSans-SemiBold.woff2')", async () => {
-    return validate(
+    return await validate(
       'OpenSans-SemiBold.woff2',
       true,
       new RegExp('OpenSans-SemiBold.woff2'),
@@ -36,86 +40,90 @@ describe('Validate Font File Name', () => {
   });
 
   spec("regex: 'OpenSans-SemiBold.woff2'", async () => {
-    return validate('OpenSans-SemiBold.woff2', true, 'OpenSans-SemiBold.woff2');
+    return await validate(
+      'OpenSans-SemiBold.woff2',
+      true,
+      'OpenSans-SemiBold.woff2',
+    );
   });
 
   spec('Open-SemiBold.woff2', async () => {
-    return validate('Open-SemiBold.woff2', true);
+    return await validate('Open-SemiBold.woff2', true);
   });
 
   spec('src/fonts/OpenSans-SemiBold.woff2', async () => {
-    return validate('src/fonts/OpenSans-SemiBold.woff2', true);
+    return await validate('src/fonts/OpenSans-SemiBold.woff2', true);
   });
 
   spec('Open-Sans-SemiBold.woff2', async () => {
-    return validate('Open-Sans-SemiBold.woff2', false);
+    return await validate('Open-Sans-SemiBold.woff2', false);
   });
 
   spec('src/fonts/OpenSans.woff2', async () => {
-    return validate('src/fonts/OpenSans.woff2', false);
+    return await validate('src/fonts/OpenSans.woff2', false);
   });
 
   spec('OpenSans-SemiBold.unknown', async () => {
-    return validate('OpenSans-SemiBold.unknown', false);
+    return await validate('OpenSans-SemiBold.unknown', false);
   });
 
   spec('OpenSans-SemiBold.woff2.woff2', async () => {
-    return validate('OpenSans-SemiBold.woff2.woff2', false);
+    return await validate('OpenSans-SemiBold.woff2.woff2', false);
   });
 
   spec('-OpenSans-SemiBold.woff2', async () => {
-    return validate('-OpenSans-Semibold.woff2', false);
+    return await validate('-OpenSans-Semibold.woff2', false);
   });
 
   spec('OPENSANS-SemiBold.woff2', async () => {
-    return validate('OPENSANS-SemiBold.woff2', false);
+    return await validate('OPENSANS-SemiBold.woff2', false);
   });
 
   spec('OpenSans-400.woff2', async () => {
-    return validate('OpenSans-400.woff2', false);
+    return await validate('OpenSans-400.woff2', false);
   });
 
   spec('OpenSans123-SemiBold.woff2', async () => {
-    return validate('OpenSans123-SemiBold.woff2', false);
+    return await validate('OpenSans123-SemiBold.woff2', false);
   });
 
   spec('openSans-SemiBold.woff2', async () => {
-    return validate('openSans-SemiBold.woff2', false);
+    return await validate('openSans-SemiBold.woff2', false);
   });
 
   spec('OpenSans-semiBold.woff2', async () => {
-    return validate('OpenSans-semiBold.woff2', false);
+    return await validate('OpenSans-semiBold.woff2', false);
   });
 
   spec('OpenSans-Semibold.woff2', async () => {
-    return validate('OpenSans-Semibold.woff2', false);
+    return await validate('OpenSans-Semibold.woff2', false);
   });
 
   spec('OpenSans-semibold.woff2', async () => {
-    return validate('OpenSans-semibold.woff2', false);
+    return await validate('OpenSans-semibold.woff2', false);
   });
 
   spec('OpenSans-SEMIBOLD.woff2', async () => {
-    return validate('OpenSans-SEMIBOLD.woff2', false);
+    return await validate('OpenSans-SEMIBOLD.woff2', false);
   });
 
   spec('OpenSans-Semiold.woff2', async () => {
-    return validate('OpenSans-Semiold.woff2', false);
+    return await validate('OpenSans-Semiold.woff2', false);
   });
 
   spec('OpenSans-Premium-SemiBold.woff2', async () => {
-    return validate('OpenSans-Premium-SemiBold.woff2', false);
+    return await validate('OpenSans-Premium-SemiBold.woff2', false);
   });
 
   spec('OpenSans-.woff2', async () => {
-    return validate('OpenSans-.woff2', false);
+    return await validate('OpenSans-.woff2', false);
   });
 
   spec('-SemiBold.woff2', async () => {
-    return validate('-SemiBold.woff2', false);
+    return await validate('-SemiBold.woff2', false);
   });
 
   spec('OpenSans.woff2', async () => {
-    return validate('OpenSans.woff2', false);
+    return await validate('OpenSans.woff2', false);
   });
 });
